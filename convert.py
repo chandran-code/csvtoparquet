992cfbe92f33328557771a51bf8d42c9f18eefc2
chandran-code <57050228+chandran-code@users.noreply.github.com>
2021-09-18 20:07:23 +0530

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+df = pd.read_csv('D:\\Personal\\BITS\\parquet\\Cancer_Dataset.csv')
+
+print(df)
+
+df.to_parquet('D:\Personal\\BITS\\parquet\\Cancer_Dataset.txt',
+              compression='None')
+              
+pd.read_parquet('D:\\Personal\\BITS\\parquet\\Cancer_Dataset.txt')
